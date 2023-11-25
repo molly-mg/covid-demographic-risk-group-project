ea052fba60a067446be8b0988516ff182036ed15
molly-mg <mmgood2222@gmail.com>
2023-11-25 18:10:45 +0000

    Began investigating the data

--- scripts/01_good_figure.R
+++ scripts/01_good_figure.R
@@ -1,1 +1,30 @@
+# PACKAGES ----
 library(tidyverse)
+#__________________________----
+
+# IMPORT DATA
+source('scripts/clean_data.R')
+# Data is imported as covid
+
+colnames(covid)
+
+#covid <- na.omit(covid)
+
+
+#covid <- covid %>% 
+#  mutate(hospitalized = recode(hospitalized, "No" = 0, "Yes" = 1)
+
+
+
+
+
+summarize(
+  group_by(filter(
+    covid, is.na(case_age) == FALSE, is.na(hospitalized) == FALSE
+  ), died, hospitalized),
+  count=n(),
+  avg_age=mean((case_age))
+)
+
+
+
