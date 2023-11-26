ecf524cfa76e13143755ecb993061b8120bb9986
Isobel-Banks <mwz21vnu@uea.ac.uk>
2023-11-26 17:46:45 +0000

    Working on graphs to see if there is much difference between the symptoms

--- scripts/01_banks_figure_script.R
+++ scripts/01_banks_figure_script.R
@@ -57,6 +57,74 @@ symptom_data_yes <- filter(.data = symptom_data, sym_myalgia == "Yes",
 head(symptom_data_yes) # Check to see if the right data is included
 
 
-#Create graph ----
-sym_box_plot <- ggplot(aes(x=))
+filter_mygalia_sym <- filter(.data = symptom_data, sym_myalgia == "Yes")
 
+#Create graph ----
+
+### Mygalia ------
+sym_box_plot_mygalia <- filter_mygalia_sym %>%
+  ggplot(aes(x= sym_myalgia,
+             y= case_age,
+             ))+
+  geom_violin(alpha = 0.15,
+              colour = "#332288",
+              fill = "#332288")+
+  geom_boxplot(width = 0.2,
+               alpha = 0.7,
+               colour = "#332288",
+               fill = "#332288")+
+  coord_flip()+ # flips to make the graph horizontal
+  theme_minimal()+
+  theme(axis.text.y=element_blank() # removes the labelled yes value of y axis
+        )
+
+plot(sym_box_plot_mygalia)
+
+
+filter_cough_sym <- filter(.data = symptom_data, sym_cough == "Yes")
+
+sym_box_plot_cough <- filter_cough_sym %>%
+  ggplot(aes(x= sym_cough,
+             y= case_age,
+  ))+
+  geom_violin(alpha = 0.15,
+              colour = "#88CCEE",
+              fill = "#88CCEE")+
+  geom_boxplot(width = 0.2,
+               alpha = 0.7,
+               colour = "#88CCEE",
+               fill = "#88CCEE")+
+  coord_flip()+ # flips to make the graph horizontal
+  theme_minimal()+
+  theme(axis.text.y=element_blank() # removes the labelled yes value of y axis
+  )
+
+plot(sym_box_plot_cough)
+
+
+### Fever -----
+filter_fever_sym <- filter(.data = symptom_data, sym_fever == "Yes")
+
+sym_box_plot_fever <- filter_fever_sym %>%
+  ggplot(aes(x= sym_fever,
+             y= case_age,
+  ))+
+  geom_violin(alpha = 0.15,
+              colour = "#44AA99",
+              fill = "#44AA99")+
+  geom_boxplot(width = 0.2,
+               alpha = 0.7,
+               colour = "#44AA99",
+               fill = "#44AA99")+
+  coord_flip()+ # flips to make the graph horizontal
+  theme_minimal()+
+  theme(axis.text.y=element_blank() # removes the labelled yes value of y axis
+  )
+
+plot(sym_box_plot_fever)
+
+
+
+
+sym_three_combined <- sym_box_plot_mygalia / sym_box_plot_cough / sym_box_plot_fever
+plot(sym_three_combined)
